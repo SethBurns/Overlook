15aa6df236b66e65d79da9697a0ed21c1ed5d703
Seth Burns <seth@sethburns.com>
2023-06-07 13:35:07 -0700

    Add initial HTML

--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,8 @@
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-
+import './images/favicon.ico'
+import datepicker from 'js-datepicker'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
